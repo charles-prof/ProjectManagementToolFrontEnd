df06a2c21dab51813bd2daa21802492ba1282d5b
Charles Kumar <gamescharlie79@gmail.com>
2025-12-02 12:12:53 +0530

    config: add railway backend public url
    * springboot backend url ( ipv4 public )

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "projectmanagementtoolbackend-production-86c3.up.railway.app";
 
 const api = axios.create({ baseURL: API_BASE_URL });
 
