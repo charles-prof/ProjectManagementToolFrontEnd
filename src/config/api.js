61ab930796d10385fdfd4765b0af0f321ebe8c3a
Charles Kumar <gamescharlie79@gmail.com>
2025-12-04 15:53:42 +0530

    chore: add .env secrets to .gitignore

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-export const API_BASE_URL = "projectmanagementtoolbackend-production-86c3.up.railway.app";
+// export const API_BASE_URL = "projectmanagementtoolbackend-production-86c3.up.railway.app";
+export const API_BASE_URL = import.meta.env.VITE_BASE_URL;
 
-const api = axios.create({ baseURL: API_BASE_URL });
+const api = axios.create({ baseURL: import.meta.env.VITE_BASE_URL, withCredentials: true });
 
 // Interceptor ensures latest token is always picked up
 api.interceptors.request.use(
