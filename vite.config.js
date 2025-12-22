81a3ed452a66fb917fc868be3f6c851b95538386
Charles Kumar <gamescharlie79@gmail.com>
2025-12-22 19:21:28 +0530

    chore: add secrets to gitignoe
    * use dotenv for project secrets managment

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
-// https://vite.dev/config/
+const env = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-   resolve: {
+  plugins: [react(), tailwindcss()],
+  resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
     cors: {
-      origin: import.meta.env.VITE_API_URL, 
+      origin: env.API_URL,
       credentials: true,
     },
   },
-})
+});
